a94ce28f3118bc1be3d403f9a464894c3079e27f
zaki <zakaprog@gmail.com>
2026-01-02 13:37:02 +0100

    fixing colorscheme , lualine confusion

--- lua/zaki/plugins/colorscheme.lua
+++ lua/zaki/plugins/colorscheme.lua
@@ -1,32 +1,14 @@
 return {
-
-  -- Tokyo Night (default)
   {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("tokyonight")
+      require("zaki.colors").load()
     end,
   },
 
-  -- Catppuccin (very clean, modern)
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 900,
-  },
-
-  -- Gruvbox (classic, very readable)
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 800,
-  },
-
-  -- OneDark (VSCode-like)
-  {
-    "navarasu/onedark.nvim",
-    priority = 700,
-  },
-
+  { "catppuccin/nvim", name = "catppuccin", priority = 900 },
+  { "ellisonleao/gruvbox.nvim", priority = 800 },
+  { "navarasu/onedark.nvim", priority = 700 },
 }
 
--- lua/zaki/plugins/lualine.lua
+++ lua/zaki/plugins/lualine.lua
@@ -1,16 +1,23 @@
 return {
-
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-      require("zaki.colors").load()
-    end,
-  },
-
-  { "catppuccin/nvim", name = "catppuccin", priority = 900 },
-  { "ellisonleao/gruvbox.nvim", priority = 800 },
-  { "navarasu/onedark.nvim", priority = 700 },
-
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "│", right = "│" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "diagnostics", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
 
